da5eed4f361583cc741c17a3b2d2c37e189634c9
Yoel Melki <ymelki@gmail.com>
2022-04-01 09:59:31 +0200

    first commit

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function index(Request $request): Response
+    {
+        // on créé un formulaire de type contact Form/ContactType
+        $formulaire=$this->createForm(ContactType::class);
+        //Lit les données envoyé via l'url
+        $formulaire->handleRequest($request);
+
+        // on vérifie si les donnée sont envoyé
+        if ($formulaire->isSubmitted()){
+
+            // on recuperer les donnée envoyé
+            $data=$formulaire->getData();
+            // on redirige vers la page envoye.html.twig
+            // avec la variable data['nom']
+            return $this->renderForm('contact/envoye.html.twig', [
+                'data' => $data['nom']
+            ]);
+        }
+        // si non on renvoie vers la page de contact avec le form
+        else {
+            return $this->renderForm('contact/index.html.twig', [
+                'form' => $formulaire
+            ]);
+        }
+
+    }
+}
